d8f24db39e69c0e806dcf10859b531d218df7ee3
PhuongNam <nguyenducnam.it@gmail.com>
2022-07-25 17:41:30 +0700

    fix css name, format file

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,43 @@ import routes from '@/routes';
 import { uuid } from '@/utils/commonFun';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <nav className="nav">
-          {routes.map((route) => {
-            const { path, name, hidden } = route;
-            return (
-              !hidden && (
-                <NavLink key={uuid()} to={path} className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
-                  {name}
-                </NavLink>
-              )
-            );
-          })}
-        </nav>
-        <div className="wraper">
-          <ErrorBoundaryPage>
-            <Suspense fallback={<div>Loading...</div>}>
-              <Routes>
-                {routes.map((route) => {
-                  const { path, component: Component } = route;
-                  return <Route key={uuid()} path={path} element={<Component />} />;
-                })}
-              </Routes>
-            </Suspense>
-          </ErrorBoundaryPage>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<nav className='nav'>
+					{routes.map((route) => {
+						const { path, name, hidden, children } = route;
+						return (
+							!hidden && (
+								<NavLink key={uuid()} to={path} className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
+									{name}
+								</NavLink>
+							)
+						);
+					})}
+				</nav>
+				<div className='wrapper'>
+					<ErrorBoundaryPage>
+						<Suspense fallback={<div>Loading...</div>}>
+							<Routes>
+								{routes.map((route) => {
+									const { path, component: Component, children } = route;
+									return (
+										<Route key={uuid()} path={path} element={<Component />}>
+											{children?.map((routeChild) => {
+												const { path: pathChild, component: ComponentChild } = routeChild;
+												return <Route key={uuid()} path={pathChild} element={<ComponentChild />} />;
+											})}
+										</Route>
+									);
+								})}
+							</Routes>
+						</Suspense>
+					</ErrorBoundaryPage>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
